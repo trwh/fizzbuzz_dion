b096d38c9656a2c272e5c07b063a74a6fa1a3ae1
deonjtan@gmail.com <deonjtan@gmail.com>
2015-10-08 10:55:42 +0100

    Tests for % 5 green - added tests for three and five.

--- Fizzbuzz.rb
+++ Fizzbuzz.rb
@@ -4,4 +4,8 @@ class Fizzbuzz
     num % 3 == 0
   end
 
+  def is_divisible_by_five?(num)
+    num % 5 == 0
+  end
+
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -18,5 +18,12 @@ describe Fizzbuzz do
       expect(subject.is_divisible_by_five?(11)).to be_falsey
   end
 
+  it 'knows that a number is divisible by 3 and 5' do
+      expect(subject.is_divisible_by_three_and_five?(15)).to be_truthy
+  end
+
+  it 'knows that a number is not divisible by 3 and 5' do
+      expect(subject.is_divisible_by_three_and_five?(21)).to be_falsey
+  end
 
 end
